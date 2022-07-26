6182188b2b05eea02b06537b252373c97bb97c86
Jason Chun <jchun711@gmail.com>
2022-07-26 17:22:43 -0400

    Updates

---
+++ precache-manifest.9688dfbaa55b604b7181c210ba7e2e06.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "1c0a57b898d8771c8021",
+    "url": "/hw-20-react-portfolio/static/css/main.86947cd0.chunk.css"
+  },
+  {
+    "revision": "1c0a57b898d8771c8021",
+    "url": "/hw-20-react-portfolio/static/js/main.63882519.chunk.js"
+  },
+  {
+    "revision": "2aaabaf61b2e02f343f0",
+    "url": "/hw-20-react-portfolio/static/js/runtime~main.d552ff96.js"
+  },
+  {
+    "revision": "68eba1b6078a94537f6a",
+    "url": "/hw-20-react-portfolio/static/css/2.5014cafc.chunk.css"
+  },
+  {
+    "revision": "68eba1b6078a94537f6a",
+    "url": "/hw-20-react-portfolio/static/js/2.4f4d31c6.chunk.js"
+  },
+  {
+    "revision": "a03a2b79c7dfa4b66d3d",
+    "url": "/hw-20-react-portfolio/static/js/3.7fa365a8.chunk.js"
+  },
+  {
+    "revision": "8d7115528f6745c331ab6cad18488db8",
+    "url": "/hw-20-react-portfolio/index.html"
+  }
+];
